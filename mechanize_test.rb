8483beb59e8633d3354e6f607a3c356f595d542f
Kevin Yeh <null>
2014-02-08 17:27:08 -0800

    Add csv files and mechanize script

--- mechanize_test.rb
+++ mechanize_test.rb
@@ -4,26 +4,27 @@ require 'nokogiri'
 require 'open-uri'
 require File.expand_path('../config/environment',__FILE__)
 
-p (Building.new().inspect)
+aFile = File.new('building_urls.csv', 'w')
 
-a = Mechanize.new { |agent|
-  agent.user_agent_alias = 'Mac Safari'
-}
-response = a.get('http://www.google.com/imghp')
-form = response.forms.first
-form_field = form.field_with(name: 'q').value = "sears tower"
-search_result = form.submit
+Building.all.each do |building|
+  a = Mechanize.new { |agent|
+    agent.user_agent_alias = 'Mac Safari'
+  }
+  response = a.get('http://www.google.com/imghp')
 
+  form = response.forms.first
+  form_field = form.field_with(name: 'q').value = building.name
+  search_result = form.submit
 
+  # building_url = search_result.search(".rg_di").first.children.first.attribute("src")
 
-puts search_result.search(".rg_di").first.children.first.attribute("src")
-# p search_result.uri
+  # unless building_url
+    building_url = search_result.search(".rg_di").first.children.first.search("img").attribute("src")
+  # end
 
-# PAGE_URL = 'https://www.google.com/search?site=imghp&tbm=isch&source=hp&biw=916&bih=334&q=sears+tower&oq=sears+tower&gs_l=img.3..0l10.2270081.2271621.0.2271737.11.9.0.2.2.0.121.571.8j1.9.0....0...1ac.1.34.img..0.11.581.teUvWS1tL2E#imgdii=_'
-# page = Nokogiri::HTML(open(PAGE_URL))
+  new_string = "\"#{building.name}\"".concat(",#{building_url}").concat("\n")
 
-# p 'A'*50
-# p page
-# p 'B'*50
-# p page.css("div[data-ri='0']")
-# p 'C'*50
+  print new_string
+  aFile.syswrite(new_string)
+end
+aFile.close
