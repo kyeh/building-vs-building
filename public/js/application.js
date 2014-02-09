8ba5cc556e249b4fd8a85e070f949401cc8bbc40
Kevin Yeh <null>
2014-02-08 19:24:03 -0800

    Added chart js library

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  //Get the context of the canvas element we want to select
+  //Get context with jQuery - using jQuery's .get() method.
+  if ($("myChart").length != 0) {
+    console.log("this is running")
+    var ctx = $("#myChart").get(0).getContext("2d");
+    //This will get the first returned node in the jQuery collection.
+    var data = {
+      labels: ["building 1", "building 2"],
+      datasets: [
+        {
+          fillColor:"rgba(220,220,220,0.5)",
+          strokeColor : "rgba(220,220,220,1)",
+          data : [65,59]
+        }
+      ]
+    }
+    var myNewChart = new Chart(ctx).Bar(data,{}});
+  }
 });
