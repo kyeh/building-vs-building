618d7c4285aa3e40a567202f5be6243556ac5d4d
Brantley Beaird <bbeaird@gmail.com>
2014-02-08 20:02:43 -0800

    Removed bands related stuff.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -31,16 +31,6 @@ get '/' do
   erb :drag_and_drop
 end
 
-get '/bands' do
-  @band_names = Band.all.map(&:name)
-  erb :bands
-end
-
-post '/bands' do
-  new_band = Band.create!(name: params[:name])
-  new_band.name
-end
-
 get '/info' do
   Demo.new(self).info
 end
